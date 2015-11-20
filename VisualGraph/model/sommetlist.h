56b1de17d913cac80bdd4d356111d10e39b6bda5
Seymour49 <ugo.rayer@laposte.net>
2015-11-20 15:44:55 +0100

    modif

--- VisualGraph/model/sommetlist.h
+++ VisualGraph/model/sommetlist.h
@@ -6,6 +6,47 @@
 #include <iterator>
 #include <cstdlib>
 
+using namespace std;
+
+class SommetList {
+private:
+    int id;
+    list<SommetList *> adjacents;
+
+public:
+    SommetList(int x):id(x),adjacents(null){
+    }
+
+    ~SommetList(){
+        adjacents.clear();
+    }
+    const int get_id(){ return id; };
+    list<SommetList*>& get_adjacents(){ return adjacents; };
+
+     ostream& print(ostream& out){
+
+      out << "Sommet " << id << " : " ;
+
+      for(list<Sommet *>::iterator it = adjacents.begin();it != adjacents.end();++it){
+              out << (*it).get_id() << ";";
+      }
+      out << endl;
+
+
+      return out;
+      }
+
+
+      friend ostream& operator<<(ostream& out, SommetList& s){
+      return s.print(out);
+      }
+
+      void updateArc(SommetList* dest){
+            // On doit ajouter un pointeur
+            adjacents.push_back(dest);
+      }
+
+};
 
 #endif // SOMMETLIST_H
 
