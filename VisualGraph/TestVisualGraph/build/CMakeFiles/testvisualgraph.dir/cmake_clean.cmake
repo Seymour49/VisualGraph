95bdb3d56a3539a24a46beda77e41a37b780e87d
flodavid <fl.david.53@gmail.com>
2015-11-27 16:42:20 +0100

    ajout création

---
+++ VisualGraph/TestVisualGraph/build/CMakeFiles/testvisualgraph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testvisualgraph.dir/main.o"
+  "CMakeFiles/testvisualgraph.dir/home/florian/Projects/VisualGraph/VisualGraph/model/graphemat.o"
+  "CMakeFiles/testvisualgraph.dir/home/florian/Projects/VisualGraph/VisualGraph/model/sommetMat.o"
+  "testvisualgraph.pdb"
+  "testvisualgraph"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testvisualgraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ VisualGraph/TestVisualGraph/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "../model/graphemat.h"
+
+using namespace std;
+
+bool testLoading(int argc, char** argv, GrapheMat* g){
+    cout << "argc : "<< argc<< endl;
+    if (argc>1){
+	    
+	if(g->tryLoadFile(argv[1])){
+	    cout <<(*g) << endl;
+	}
+	else{
+	    cout << "Erreur de fichier" << endl;
+	    return false;
+	}
+    //     delete(g);
+    } else {
+	cout<< "Entrez le chemin d'un fichier de clique :"<< endl;
+	string filename;
+	cin >> filename;
+	if(g->tryLoadFile(filename)){
+	    cout <<(*g) << endl;
+	}
+	else{
+	    cout << "Mauvais fichier" << endl;
+	    return false;
+	}
+    }
+    
+    return true;
+}
+
+int main(int argc, char **argv) {
+
+    GrapheMat* g = new GrapheMat("Test");
+    
+    bool successTestLoad= testLoading(argc, argv, g);
+    if (successTestLoad) cout<< "Test Chargement fichier réussi"<< endl;
+    else cout<< "Test Chargement fichier échoué"<< endl;
+    
+    return 0;
+}
