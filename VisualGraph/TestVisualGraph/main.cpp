0fd6a0a9dd10dd555bd318088ad4e66eb7ed9dfb
Seymour49 <ugo.rayer@laposte.net>
2015-11-30 21:38:40 +0100

    Test set_intersection KO

--- VisualGraph/TestVisualGraph/main.cpp
+++ VisualGraph/TestVisualGraph/main.cpp
@@ -40,7 +40,7 @@ bool testLoading(int argc, char** argv, GrapheMat* g){
 bool testIntersection(GrapheMat* g1, GrapheMat* g2){
   
   
-  
+  return true;
   
 }
 
@@ -63,24 +63,47 @@ int main(int argc, char **argv) {
     /* DONE
      * 	tests valgrind ok sur création et suppression
      * TODO 
-     * Tester intersection via std::set_intersection(v.begin(),v.end(), v2.begin(), v2.end(), vecteur resultat);
-     * 
+     * KO Tester intersection via std::set_intersection(v.begin(),v.end(), v2.begin(), v2.end(), vecteur resultat); 
+     * Définir intersection
      */
     GrapheMat* g1 = new GrapheMat("G1");
-    GrapheMat* g2 = new GrapheMat("G2");
     
-    string file1 = "../../model/testGraphe10.txt";
-    string file2 = "../../model/testGraphe20.txt";
+    string file = "../model/testGraphe25.txt";
+    cout << file << endl;
     
-    if(g1->tryLoadFile(file1) && g2->tryLoadFile(file2)){
+    if(g1->tryLoadFile(file) ){
 	cout << "Chargement réussi " << endl;
+	// Découpage du graphe en différents vecteurs de sommets
+	vector<SommetMat *> v1;
+	vector<SommetMat *> v2;
+	
+	for(int i=1; i<g1->getNbSommets();++i){
+	    v1.push_back(g1->at(i));
+	    if(i%3 == 0)
+	      v2.push_back(g1->at(i));
+	}
+	cout << "V1" << endl;
+	for(auto n : v1 )
+	    cout << *n ;
+	cout << "V2" << endl;
+	for(auto n : v2 )
+	    cout << *n ;
+	
+	vector<SommetMat *> v3;
+	/*
+	 *  Ne fonctionne pas;
+	 */
+	//set_intersection(v1.begin(),v1.end(),v2.begin(),v2.end(), v3);
+	
+	cout << "V3" << endl;
+	for(auto n : v3)
+	    cout << *n ;
       
     }else{
 	cout << "Et merde..." << endl;
     }
     
     delete(g1);
-    delete(g2);
     
     string dummy;
     cin >>dummy;
