8abe09c32b62ed11d1340547d7b47e7752e4ea64
Seymour49 <ugo.rayer@laposte.net>
2015-12-01 23:15:31 +0100

    Mise en place et test Union

--- VisualGraph/TestVisualGraph/main.cpp
+++ VisualGraph/TestVisualGraph/main.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
+#include <boost/iterator/iterator_concepts.hpp>
 
 #include "../model/graphemat.h"
 
 using namespace std;
+
+
+/**
+ * Fonction d'intersection entre deux vecteurs de sommets.
+ * @param const_vector<T*> v1 @param const_vector<T*> v2
+ * @return vector<T*>
+ */
+vector<SommetMat *> setIntersection(const vector<SommetMat* > &v1,const vector<SommetMat *> &v2){
+    vector<SommetMat *> res;
+    
+    for(unsigned int i = 0; i< v1.size() ; ++i ){
+	for(unsigned int j = 0; j< v2.size(); ++j ){
+	    if( v1.at(i)->get_id() == v2.at(j)->get_id() )
+	      res.push_back(v1.at(i)); 
+	}
+      
+    }  
+    return res;
+}
+
+vector<SommetMat *> setIntersection(const vector<SommetMat *> &v1,SommetMat* s){
+  
+    vector<SommetMat *> res;
+    
+    for(unsigned int i = 0; i< v1.size() ; ++i ){
+	
+	  if( v1.at(i)->get_id() == s->get_id() )
+		res.push_back(v1.at(i)); 
+      
+    }  
+    return res;
+  
+}
+/**
+ * Fonction d'union entre deux vecteurs de sommets.
+ * @param const_vector<T*> v1 @param const_vector<T*> v2
+ * @return vector<T*>
+ */
+vector<SommetMat *> setUnion(const vector<SommetMat* > &v1,const vector<SommetMat *> &v2){
+    vector<SommetMat *> res;
+    /* Principe :
+     * 
+     *	On copie dans res le plus volumineux des vecteurs
+     * 	Ensuite, pour chaque élément du plus petit, on 
+     *  teste l'intersection avec res. Si elle est nulle,
+     * 	on ajoute l'élément à res
+     *
+     */ 
+    
+    if(v1.size() > v2.size() ){
+	res = v1;
+	for(unsigned int i=0; i < v2.size() ; ++i)
+	    if(setIntersection(res, v2.at(i)).size() == 0)
+		res.push_back(v2.at(i));	
+    }else{
+	res = v2;
+	for(unsigned int i=0; i < v1.size() ; ++i)
+	    if(setIntersection(res, v1.at(i)).size() == 0)
+		res.push_back(v1.at(i));	
+    }
+
+    return res;
+}
+
+
 
 bool testLoading(int argc, char** argv, GrapheMat* g){
     cout << "argc : "<< argc<< endl;
@@ -37,50 +103,17 @@ bool testLoading(int argc, char** argv, GrapheMat* g){
     return true;
 }
 
-bool testIntersection(GrapheMat* g1, GrapheMat* g2){
-  
-  
-  return true;
-  
-}
-
-int main(int argc, char **argv) {
-    /**
-     * Test de chargement 
-     * 
-    GrapheMat* g = new GrapheMat("Test");
+bool testIntersection(int argc, char** argv, GrapheMat* g){
     
-    bool successTestLoad= testLoading(argc, argv, g);
-    if (successTestLoad) cout<< "Test Chargement fichier réussi"<< endl;
-    else cout<< "Test Chargement fichier échoué"<< endl;
-    
-    */
-    
-    /*
-     * Test intersection
-     */
-    
-    /* DONE
-     * 	tests valgrind ok sur création et suppression
-     * TODO 
-     * KO Tester intersection via std::set_intersection(v.begin(),v.end(), v2.begin(), v2.end(), vecteur resultat); 
-     * Définir intersection
-     */
-    GrapheMat* g1 = new GrapheMat("G1");
-    
-    string file = "../model/testGraphe25.txt";
-    cout << file << endl;
-    
-    if(g1->tryLoadFile(file) ){
-	cout << "Chargement réussi " << endl;
+    if(g->tryLoadFile("../model/testGraphe25.txt") ){
 	// Découpage du graphe en différents vecteurs de sommets
 	vector<SommetMat *> v1;
 	vector<SommetMat *> v2;
 	
-	for(int i=1; i<g1->getNbSommets();++i){
-	    v1.push_back(g1->at(i));
+	for(int i=1; i<=g->getNbSommets();++i){
+	    v1.push_back(g->at(i));
 	    if(i%3 == 0)
-	      v2.push_back(g1->at(i));
+	      v2.push_back(g->at(i));
 	}
 	cout << "V1" << endl;
 	for(auto n : v1 )
@@ -89,21 +122,92 @@ int main(int argc, char **argv) {
 	for(auto n : v2 )
 	    cout << *n ;
 	
-	vector<SommetMat *> v3;
 	/*
-	 *  Ne fonctionne pas;
+	 * Fonctionne, reste à faire test performance
 	 */
-	//set_intersection(v1.begin(),v1.end(),v2.begin(),v2.end(), v3);
+	vector<SommetMat *> v3 = setIntersection(v1,v2);
 	
 	cout << "V3" << endl;
 	for(auto n : v3)
 	    cout << *n ;
       
     }else{
-	cout << "Et merde..." << endl;
+	cout << "Erreur de chargement du fichier de test" << endl;
+	return false;
     }
+  
+    return true;
+}
+
+
+bool testUnion(GrapheMat* g1, GrapheMat* g2){
     
-    delete(g1);
+
+    if( g1->tryLoadFile("../model/testGraphe25.txt") ){
+	if( g2->tryLoadFile("../model/testGraphe.txt")){
+	  
+	  vector<SommetMat *> v1;
+	  vector<SommetMat *> v2;
+	  for(int i=1; i <= g1->getNbSommets(); ++i)
+	      v1.push_back(g1->at(i));
+	  
+	  for(int i=1; i <= g2->getNbSommets(); ++i)
+	      v2.push_back(g2->at(i));
+	  
+	  
+	  
+	  vector<SommetMat* > v3= setUnion(v1,v2);
+	  
+	  for(auto n : v3)
+	      cout << *n ;
+	  
+	}else{
+	    cout << "Error while loading ../model/testGraphe.txt" << endl;
+	}
+    }else{
+	cout << "Error while loading ../model/testGraphe25.txt" << endl;
+	return false;
+    }
+  
+  
+ 
+    return true;
+}
+
+
+int main(int argc, char **argv) {
+
+    GrapheMat* g1 = new GrapheMat("Test");
+     /**
+     * Test de chargement 
+     */
+    /*
+    if (testLoading(argc, argv, g1))
+      cout<< "Test Chargement fichier réussi"<< endl;
+    else 
+      cout<< "Test Chargement fichier échoué"<< endl;
+    */
+    
+    /*
+     * Test intersection
+     */
+    if(testIntersection(argc,argv,g1))
+	cout << "Fin de test Intersection : Succès" << endl;
+    else
+	cout << "Fin de test Intersection : Echec" << endl;
+
+    /**
+     * Test Union : Fonctionne à priori, demande de vérification de l'algo pour Flo par Ugo
+     */
+     GrapheMat* g2 = new GrapheMat("Test2");
+     if(testUnion(g1,g2))
+	cout << "Fin test Union : Succès" << endl;
+     else
+	cout << "Fin test Union : Echec" << endl;
+     
+     
+      delete(g1);
+      delete(g2);
     
     string dummy;
     cin >>dummy;
